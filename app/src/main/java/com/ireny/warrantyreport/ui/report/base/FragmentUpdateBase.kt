5afe39a50a13b3efac40cf6572728e1a32f2466b
Ireny Escobar <ireny.escobar@lab3dvlp.com>
2019-11-15 00:45:12 -0400

    added FragmentUpdateBase

---
+++ app/src/main/java/com/ireny/warrantyreport/ui/report/base/FragmentUpdateBase.kt
@@ -0,0 +1,6 @@
+package com.ireny.warrantyreport.ui.report.base
+
+import com.ireny.warrantyreport.entities.Report
+import com.ireny.warrantyreport.ui.report.interfaces.IUpdateFragment
+
+abstract class FragmentUpdateBase :FragmentBase(), IUpdateFragment<Report>
