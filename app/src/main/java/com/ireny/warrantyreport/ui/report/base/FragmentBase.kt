fc075073ee6a55ed2de3674fba50ee1520c0b925
Ireny Escobar <ireny.escobar@lab3dvlp.com>
2019-11-15 00:52:59 -0400

    removed IUpdateFragment implementation of FragmentBase class

--- app/src/main/java/com/ireny/warrantyreport/ui/report/base/FragmentBase.kt
+++ app/src/main/java/com/ireny/warrantyreport/ui/report/base/FragmentBase.kt
@@ -3,6 +3,5 @@ package com.ireny.warrantyreport.ui.report.base
 import androidx.fragment.app.Fragment
 import com.ireny.warrantyreport.entities.Report
 import com.ireny.warrantyreport.ui.report.interfaces.IRefreshFragment
-import com.ireny.warrantyreport.ui.report.interfaces.IUpdateFragment
 
-abstract class FragmentBase: Fragment(),IRefreshFragment<Report>, IUpdateFragment<Report>
+abstract class FragmentBase: Fragment(),IRefreshFragment<Report>
