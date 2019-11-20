427906fad2eaf4f92778cb5e253ab763a532072c
Ireny Escobar <ireny.escobar@lab3dvlp.com>
2019-11-20 17:00:22 -0400

    added and update helpers files

---
+++ app/src/main/java/com/ireny/warrantyreport/utils/ModelHelper.kt
@@ -0,0 +1,28 @@
+package com.ireny.warrantyreport.utils
+
+import com.ireny.warrantyreport.entities.Report
+
+fun Report.copy():Report{
+
+    return Report(
+        this.id,
+        this.reportTypeId,
+        this.companyId,
+        this.distributor,
+        this.client,
+        this.cityState,
+        this.partReference,
+        this.sourceInvoice,
+        this.invoiceDate,
+        this.applicationDate,
+        this.warrantyDate,
+        this.reasonUnfounded,
+        this.comments,
+        this.technicalConsultant,
+        this.technicalConsultantContact,
+        this.analysisDate,
+        this.created_at,
+        this.tecnicalAdvices,
+        this.reportType,
+        this.company)
+}
