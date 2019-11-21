421fd58697628585faa47fb2ec4d8c184ed85458
Ireny Escobar <ireny.escobar@lab3dvlp.com>
2019-11-20 22:09:53 -0400

    added renamed class

---
+++ app/src/main/java/com/ireny/warrantyreport/di/components/ReportDirectoryComponent.kt
@@ -0,0 +1,12 @@
+package com.ireny.warrantyreport.di.components
+
+import android.content.Context
+import com.ireny.warrantyreport.di.modules.ReportDirectoryModule
+import com.ireny.warrantyreport.ui.report.services.ReportDirectoryManager
+import dagger.Component
+
+@Component(modules = [ReportDirectoryModule::class])
+interface ReportDirectoryComponent {
+    fun inject(context: Context)
+    fun reportDirectoryManager(): ReportDirectoryManager
+}
