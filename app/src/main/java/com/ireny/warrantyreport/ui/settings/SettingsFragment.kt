6007ac62f79b8d1e0eaf646291b7fb0dc4f0bba6
Ireny Escobar <ireny.escobar@lab3dvlp.com>
2019-11-20 23:26:31 -0400

    cleaned code

--- app/src/main/java/com/ireny/warrantyreport/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/ireny/warrantyreport/ui/settings/SettingsFragment.kt
@@ -4,15 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.ireny.warrantyreport.R
 
-class NotificationsFragment : Fragment() {
+class SettingsFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
+    private lateinit var notificationsViewModel: SettingsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,12 +18,7 @@ class NotificationsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         notificationsViewModel =
-            ViewModelProviders.of(this).get(NotificationsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_settings, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        notificationsViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
-        return root
+            ViewModelProviders.of(this).get(SettingsViewModel::class.java)
+        return inflater.inflate(R.layout.fragment_settings, container, false)
     }
 }
--- app/src/main/java/com/ireny/warrantyreport/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/com/ireny/warrantyreport/ui/settings/SettingsViewModel.kt
@@ -1,13 +1,7 @@
 package com.ireny.warrantyreport.ui.settings
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class SettingsViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
-    }
-    val text: LiveData<String> = _text
 }
