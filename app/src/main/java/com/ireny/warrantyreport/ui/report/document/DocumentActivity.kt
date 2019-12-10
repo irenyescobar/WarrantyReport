6df0df0ab1fc4761e26fd4c851023daaaf2cdbb2
Ireny Escobar <ireny.escobar@lab3dvlp.com>
2019-12-09 21:05:27 -0400

    updated functions

--- app/src/main/java/com/ireny/warrantyreport/ui/report/document/DocumentActivity.kt
+++ app/src/main/java/com/ireny/warrantyreport/ui/report/document/DocumentActivity.kt
@@ -40,21 +40,25 @@ class DocumentActivity : AppCompatActivity(),
         }
 
         val reportId = intent.getLongExtra(REPORT_ID,0)
+        val isCreateMode = intent.getBooleanExtra(CREATE_MODE,false)
 
         viewModel = ViewModelProviders.of(this, DocumentViewModel.Companion.Factory(
             customApp,
             reportRepository,
-            reportId)
+            reportId,
+            component.api(),
+            component.userAccountManager())
         ).get(DocumentViewModel::class.java)
 
         showFragment()
 
         viewModel.model.observe(this, Observer { el ->
             el?.let {
-                if(el.code == null) {
-                    currentFragment.bindView(el)
-                }else{
+                if(isCreateMode && el.code != null){
                     currentFragment.createDocument(el)
+                }
+                else {
+                    currentFragment.bindView(el)
                 }
             }
         })
@@ -89,6 +93,7 @@ class DocumentActivity : AppCompatActivity(),
             else -> super.onOptionsItemSelected(item)
         }
     }
+
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
@@ -133,10 +138,12 @@ class DocumentActivity : AppCompatActivity(),
     }
 
     companion object {
+        const val CREATE_MODE = "CREATE_MODE"
         @JvmStatic
-        fun newInstance(context: Context, reportId:Long): Intent {
+        fun newInstance(context: Context, reportId:Long, createMode:Boolean): Intent {
             val intent = Intent(context, DocumentActivity::class.java)
             intent.putExtra(REPORT_ID,reportId)
+            intent.putExtra(CREATE_MODE,createMode)
             return  intent
         }
     }
