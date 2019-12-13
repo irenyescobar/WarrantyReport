71f00c72dbce85008df0f8a484cd9127440f5b70
Ireny Escobar <ireny.escobar@lab3dvlp.com>
2019-12-12 21:12:22 -0400

    updated object of data imports

--- app/src/main/java/com/ireny/warrantyreport/entities/WarrantReportData.kt
+++ app/src/main/java/com/ireny/warrantyreport/entities/WarrantReportData.kt
@@ -2,4 +2,6 @@ package com.ireny.warrantyreport.entities
 
 class WarrantReportData (val reportTypes: ArrayList<ReportType>,
                          val companys: ArrayList<Company>,
-                         val technicalAdvices: ArrayList<TechnicalAdvice> )
+                         val technicalAdvices: ArrayList<TechnicalAdvice>,
+                         val reports: ArrayList<Report>,
+                         val assignedTechnicalAdvices: ArrayList<AssignedTechnicalAdvice>)
