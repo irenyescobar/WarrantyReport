d9bef40e00f3d95f77b105bd1df1075ebd3ef1b1
Ireny Escobar <ireny.escobar@lab3dvlp.com>
2020-02-11 19:17:41 -0400

    renamed values of constants

--- app/src/main/java/com/ireny/warrantyreport/utils/Constants.kt
+++ app/src/main/java/com/ireny/warrantyreport/utils/Constants.kt
@@ -4,8 +4,9 @@ import java.util.*
 
 class Constants {
     companion object {
-        const val DATABASE_NAME: String = "db-warrant-report"
+        const val DATABASE_NAME: String = "dbteclauto"
         val LOCALE_BRAZIL = Locale("pt", "BR")
-        const val REPORTS_DIRECTORY = "/Laudos"
+        const val REPORTS_DIRECTORY = "laudos"
+        const val REPORTS_FILES = "arquivos"
     }
 }
